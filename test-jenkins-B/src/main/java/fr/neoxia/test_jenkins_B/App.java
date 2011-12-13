166cf455f452bd0fa327a8ca213a179145206a78
issam ghabri <issam.ghabri@neoxia.com>
2011-12-13 12:16:03 +0100

    ajout projet test-jenkins-B

---
+++ test-jenkins-B/src/main/java/fr/neoxia/test_jenkins_B/App.java
@@ -0,0 +1,13 @@
+package fr.neoxia.test_jenkins_B;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
