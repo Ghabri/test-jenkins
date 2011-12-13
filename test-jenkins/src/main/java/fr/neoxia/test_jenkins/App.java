b67072aea01f7f001367b0f6894c0532c2cf1ca6
issam ghabri <issam.ghabri@neoxia.com>
2011-12-13 11:37:05 +0100

    ajout d'un constructeur

--- test-jenkins/src/main/java/fr/neoxia/test_jenkins/App.java
+++ test-jenkins/src/main/java/fr/neoxia/test_jenkins/App.java
@@ -2,12 +2,10 @@ package fr.neoxia.test_jenkins;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public void App() {
+
+	}
 }
