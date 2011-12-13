03353525bbffab276db2a882013b24f0487b09ce
issam ghabri <issam.ghabri@neoxia.com>
2011-12-13 12:30:59 +0100

    creation des dependences entre les modules A et B et le projet parent

---
+++ test-jenkins-Parent/test-jenkins-B/src/main/java/org/test/jenkins/B/App.java
@@ -0,0 +1,13 @@
+package org.test.jenkins.B;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
