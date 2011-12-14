d135e80b0361d65b4570953944cd3f090d47e171
issam ghabri <issam.ghabri@neoxia.com>
2011-12-14 12:22:01 +0100

    add projects 1

---
+++ test-jenkins-1-A/src/main/java/fr/neoxia/test_jenkins_1_A/App.java
@@ -0,0 +1,13 @@
+package fr.neoxia.test_jenkins_1_A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test-jenkins-1-B/src/main/java/fr/neoxia/test_jenkins_1_B/App.java
@@ -0,0 +1,13 @@
+package fr.neoxia.test_jenkins_1_B;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
